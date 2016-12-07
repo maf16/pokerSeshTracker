df639b72da8794c1f10268e58667ba843b0f10e1
Vitalii Protsenko <vp412@ic.ac.uk>
2016-12-07 01:43:16 +0000

    Minor styling work, ideas as to how to implement session filtering

--- tracker/static/js/main.js
+++ tracker/static/js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
     $('.datepicker').datepicker();
     $('#sessionStats').tablesorter();
+
+    $('#sesh-submit-form').hide();
+
+    $('#reg-new-sesh').click(function () {
+        $('#sesh-submit-form').show();
+    });
+
+    $('#sesh-sumbit-close').click(function () {
+        $('#sesh-submit-form').hide();
+    });
 });
--- tracker/views.py
+++ tracker/views.py
@@ -9,11 +9,20 @@ from models import Player, Session
 @login_required(login_url='reg/login')
 @require_GET
 def index(request):
+    sessions = request.user.player.session_set.all()
+
+    '''
+    hmm do I want to pass it in or calculate in js? seems like js is a better long term option for filtering
+    'total_profit': sum(session.profit for session in sessions),
+    'total_length': sum(session.session_length for session in sessions),
+    'avg_':
+    '''
     ctx = {
         'isAuthenticated': request.user.is_authenticated,
         'user': request.user,
-        'sessions': request.user.player.session_set.all()
+        'sessions': sessions,
     }
+
     return render(request, 'tracker/home.html', ctx)
 
 
